5f68897cf86b10bf92b4fef657525738d9c01f8b
MaregaRafa2001 <MaregaRafa2001>
2020-01-27 21:50:06 -0300

    Projeto KairósTransit

---
+++ DESPACHANTE_KAIROS_TRANSIT/mdi_principal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DESPACHANTE_KAIROS_TRANSIT
+{
+    public partial class mdi_principal : Form
+    {
+        public mdi_principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
